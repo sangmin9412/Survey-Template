30d486906c5553ad06abd663528139dc8b19c464
sanmgin9412 <tkdals9048@naver.com>
2022-07-04 20:21:02 +0900

    update: 이전, 다음 버튼 수정 - 처음인 경우 이전 버튼 비활성화 마지막인 경우 다음 버튼 비활성화

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,7 +31,7 @@
     questionCard.forEach((v, i) => {
       input[i] = {
         el: v.querySelectorAll(`[${dataType}]`),
-      }
+      };
     });
 
     return {
@@ -39,25 +39,26 @@
       questionCard,
       progressBar,
       input,
-    }
-  }
+    };
+  };
 
   const inputRegex = (val, type, length) => {
     switch (type) {
       case "text":
         if (val.trim() === "") return false;
         if (length) {
-          return (val.length >= length);
+          return val.length >= length;
         } else {
-          return (val.length !== 0);
+          return val.length !== 0;
         }
       case "email":
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(val).toLowerCase());
       default:
         return undefined;
     }
-  }
+  };
 
   const SURVEY = function (options) {
     const parent = document.querySelector(`#${options.parentId}`);
@@ -70,15 +71,30 @@
       currentStep: 1,
       completedStep: 1,
       buttonClass: {},
-      ...options
-    }
+      ...options,
+    };
 
     this.setProgress = () => {
       const { progressBar } = this.obj;
       const { step, currentStep } = this.options;
 
       progressBar.style.width = `${(currentStep / step) * 100}%`;
-    }
+
+      // next, prev 버튼 활성화 처리
+      const { prev, next } = this.options.buttonClass;
+      const $prev = document.querySelector(prev);
+      const $next = document.querySelector(next);
+
+      $prev.style.display = "block";
+      $next.style.display = "block";
+
+      if (currentStep === 1) {
+        $prev.style.display = "none";
+      }
+      if (currentStep === step) {
+        $next.style.display = "none";
+      }
+    };
 
     this.setQuestion = () => {
       const { question, questionCard, input } = this.obj;
@@ -96,7 +112,7 @@
         v.classList.remove("prev");
         v.classList.remove("next");
 
-        input[i].el.forEach(v => {
+        input[i].el.forEach((v) => {
           if (v.dataset.inputType !== "submit") {
             v.dataset.cardId = i;
           }
@@ -115,13 +131,13 @@
       question.style.minHeight = `${_height}px`;
 
       // create an Observer instance
-      const resizeObserver = new ResizeObserver(entries => {
+      const resizeObserver = new ResizeObserver((entries) => {
         question.style.minHeight = `${entries[0].target.clientHeight}px`;
       });
 
       // start observing a DOM node
-      resizeObserver.observe(questionCard[_currentStep])
-    }
+      resizeObserver.observe(questionCard[_currentStep]);
+    };
 
     this.functions = {
       InputHandler: (target) => {
@@ -144,12 +160,18 @@
         const { input } = this.obj;
         const { InputHandler, CheckboxHandler } = this.functions;
         const validArr = [];
-        const $input = Array.from(input[idx].el).filter(v => {
-          return v.dataset.inputType === "text" || v.dataset.inputType === "email" ? v : null;
+        const $input = Array.from(input[idx].el).filter((v) => {
+          return v.dataset.inputType === "text" ||
+            v.dataset.inputType === "email"
+            ? v
+            : null;
         });
 
-        const $checkbox = Array.from(input[idx].el).filter(v => {
-          return v.dataset.inputType === "checkbox" || v.dataset.inputType === "radio" ? v : null;
+        const $checkbox = Array.from(input[idx].el).filter((v) => {
+          return v.dataset.inputType === "checkbox" ||
+            v.dataset.inputType === "radio"
+            ? v
+            : null;
         });
 
         for (let i = 0; i < $input.length; i++) {
@@ -157,7 +179,9 @@
         }
 
         for (let i = 0; i < $checkbox.length; i++) {
-          CheckboxHandler($checkbox[i]) ? validArr.push(true) : validArr.push(false);
+          CheckboxHandler($checkbox[i])
+            ? validArr.push(true)
+            : validArr.push(false);
         }
 
         for (let i = 0; i < validArr.length; i++) {
@@ -169,7 +193,9 @@
       CheckboxHandler: (target) => {
         const cardId = target.dataset.cardId;
         const inputId = target.dataset.inputId;
-        const $checkbox = parent.querySelectorAll(`[data-card-id="${cardId}"][data-input-id="${inputId}"]`);
+        const $checkbox = parent.querySelectorAll(
+          `[data-card-id="${cardId}"][data-input-id="${inputId}"]`
+        );
         let result;
 
         for (let i = 0; i < $checkbox.length; i++) {
@@ -181,11 +207,11 @@
         }
 
         if (result) {
-          $checkbox.forEach(v => {
+          $checkbox.forEach((v) => {
             v.classList.remove("is-invalid");
           });
         } else {
-          $checkbox.forEach(v => {
+          $checkbox.forEach((v) => {
             v.classList.add("is-invalid");
           });
         }
@@ -196,15 +222,21 @@
         const { input } = this.obj;
         const answers = {};
         for (const key in input) {
-          const $input = Array.from(input[key].el).filter(v => {
-            return v.dataset.inputType === "text" || v.dataset.inputType === "email" ? v : null;
+          const $input = Array.from(input[key].el).filter((v) => {
+            return v.dataset.inputType === "text" ||
+              v.dataset.inputType === "email"
+              ? v
+              : null;
           });
 
-          const $checkbox = Array.from(input[key].el).filter(v => {
-            return v.dataset.inputType === "checkbox" || v.dataset.inputType === "radio" ? v : null;
+          const $checkbox = Array.from(input[key].el).filter((v) => {
+            return v.dataset.inputType === "checkbox" ||
+              v.dataset.inputType === "radio"
+              ? v
+              : null;
           });
 
-          const inputArr = $input.map(v => {
+          const inputArr = $input.map((v) => {
             const type = v.dataset.inputType;
             const id = v.dataset.inputId;
             const question = v.dataset.inputQuestion;
@@ -216,12 +248,12 @@
               question,
               value,
               type,
-            }
+            };
 
             return result;
           });
 
-          const checkboxArr = $checkbox.map(v => {
+          const checkboxArr = $checkbox.map((v) => {
             const checked = v.checked;
             const type = v.dataset.inputType;
             const id = v.dataset.inputId;
@@ -234,34 +266,38 @@
               question,
               value,
               type,
-              checked
-            }
+              checked,
+            };
 
             return result;
           });
 
           const checkboxRealArr = [];
-          checkboxArr.filter(v => v.type === "checkbox" || v.type === "radio").forEach(v => {
-            let condition = true;
-            for (let i = 0; i < checkboxRealArr.length; i++) {
-              if (checkboxRealArr[i].id === v.id) {
-                condition = false;
-                break;
-              };
-            }
-            const copy = { ...v };
-            copy.value = [];
-            delete copy.checked;
-            condition ? checkboxRealArr.push(copy) : null;
-          });
-
-          checkboxArr.filter(v => v.checked).forEach(v => {
-            for (let i = 0; i < checkboxRealArr.length; i++) {
-              if (checkboxRealArr[i].id === v.id) {
-                checkboxRealArr[i].value.push(v.value);
+          checkboxArr
+            .filter((v) => v.type === "checkbox" || v.type === "radio")
+            .forEach((v) => {
+              let condition = true;
+              for (let i = 0; i < checkboxRealArr.length; i++) {
+                if (checkboxRealArr[i].id === v.id) {
+                  condition = false;
+                  break;
+                }
               }
-            }
-          });
+              const copy = { ...v };
+              copy.value = [];
+              delete copy.checked;
+              condition ? checkboxRealArr.push(copy) : null;
+            });
+
+          checkboxArr
+            .filter((v) => v.checked)
+            .forEach((v) => {
+              for (let i = 0; i < checkboxRealArr.length; i++) {
+                if (checkboxRealArr[i].id === v.id) {
+                  checkboxRealArr[i].value.push(v.value);
+                }
+              }
+            });
 
           answers[key] = [...inputArr, ...checkboxRealArr];
 
@@ -276,24 +312,25 @@
         for (const key in answers) {
           if (Object.hasOwnProperty.call(answers, key)) {
             alertTxt += `${Number(key) + 1}번 STEP 결과 \n`;
-            answers[key].forEach(v => {
+            answers[key].forEach((v) => {
               alertTxt += `${v.id}. ${v.question} - ${v.value} \n`;
             });
             alertTxt += `-------------------------------\n`;
           }
         }
         alert(alertTxt);
-      }
-    }
+      },
+    };
 
     this.bind = {
       input: () => {
         const _this = this;
         const { input } = this.obj;
-        const { InputHandler, InputCheck, CheckboxHandler, SurveyCompleted } = this.functions;
+        const { InputHandler, InputCheck, CheckboxHandler, SurveyCompleted } =
+          this.functions;
 
         for (const key in input) {
-          input[key].el.forEach(v => {
+          input[key].el.forEach((v) => {
             const inputType = v.dataset.inputType;
 
             switch (inputType) {
@@ -318,7 +355,11 @@
 
                   if (InputCheck(_currentStep)) {
                     if (currentStep < step) {
-                      if (_this.options.currentStep === _this.options.completedStep) _this.options.completedStep++;
+                      if (
+                        _this.options.currentStep ===
+                        _this.options.completedStep
+                      )
+                        _this.options.completedStep++;
                       _this.options.currentStep++;
                       _this.setProgress();
                       _this.setQuestion();
@@ -347,9 +388,9 @@
         $prev.addEventListener("click", function () {
           const { currentStep } = _this.options;
           if (currentStep === 1) {
-            alert('처음 STEP입니다.');
-            return
-          };
+            alert("처음 STEP입니다.");
+            return;
+          }
           _this.options.currentStep--;
           _this.setProgress();
           _this.setQuestion();
@@ -362,8 +403,8 @@
           const _currentStep = currentStep - 1;
           if (currentStep === completedStep) {
             alert(`STESP.0${completedStep}번 설문을 진행해주세요.`);
-            return
-          };
+            return;
+          }
           if (InputCheck(_currentStep)) {
             _this.options.currentStep++;
             _this.setProgress();
@@ -372,7 +413,7 @@
           return false;
         });
       },
-    }
+    };
 
     return {
       init: () => {
@@ -380,9 +421,9 @@
         this.setQuestion();
         this.bind.input();
         this.bind.buttons();
-      }
-    }
-  }
+      },
+    };
+  };
 
   document.addEventListener("DOMContentLoaded", function () {
     const PMS_SURVEY = new SURVEY({
@@ -390,9 +431,9 @@
       dataType: "data-input-type",
       step: 4,
       buttonClass: {
-        prev: '.survey_1-prev',
-        next: '.survey_1-next',
-      }
+        prev: ".survey_1-prev",
+        next: ".survey_1-next",
+      },
     });
     PMS_SURVEY.init();
   });
